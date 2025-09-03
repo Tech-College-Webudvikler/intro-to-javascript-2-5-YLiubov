e09bb859a45f1f3628f3e06d461cd89732390824
YLiubov <129996599+YLiubov@users.noreply.github.com>
2025-09-03 10:12:52 +0200

    1 ex

---
+++ script.js
@@ -0,0 +1,12 @@
+const firstNumber = 5
+const secondNumber = 12
+
+let result = firstNumber + secondNumber
+
+result *= 7;
+
+console.log(result);
+
+// Hovedforskellen mellem const og let er muligheden for at tilsidesætte værdien af ​​en variabel, efter den er deklareret.
+// Nøgleordet const forhindrer, at værdien af ​​en variabel tilsidesættes.
+// Nøgleordet let tillader, at værdien af ​​en variabel ændres, efter den er deklareret.
